ba9401980164c13c6fc9f0064a4f5e46e81eeab1
Minhaz Chowdhury <minhazchowdhury@Minhazs-MacBook-Air.local>
2026-06-11 21:55:22 +0600

    inintital commit

---
+++ accounts/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomerProfile,Address
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.username = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = CustomerProfile
+        fields = ['phone','profile_image']    
+
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = '__all__'   
---
+++ products/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from shared.models import TimestampMixin
+
+# Create your models here.
+class Category(TimestampMixin):
+    category_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.category_name
+    
+class Product(TimestampMixin):
+    product_name =  models.CharField(max_length=100)
+    category = models.ForeignKey(to=Category,on_delete=models.CASCADE,related_name='product')
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5,decimal_places=2)
+
+    def __str__(self):
+        return self.product_name
+
+class ProductImage(TimestampMixin):
+    product = models.ForeignKey(to=Product,on_delete=models.CASCADE,related_name='product_images')
+    image = models.ImageField(upload_to='products/images')
+
+    def __str__(self):
+        return f"Image for {self.product.product_name}"
+    
+class ProductVariant(TimestampMixin):
+    product = models.ForeignKey(to=Product,on_delete=models.CASCADE,related_name='product_variants')
+    image = models.ImageField(upload_to='products/images')
+    color = models.CharField(max_length=100)
+    stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"Image for {self.product.product_name}"
+
+
